1ff6a6321584046609b30ecc6e8917755f19bd89
AbKadir11 <md.abdul.kadir@g.bracu.ac.bd>
2024-09-13 11:42:26 +0600

    Delete job_project directory

--- job_project/company/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CompanyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'company'
-    label = 'my_unique_company_label'
